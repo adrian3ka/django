35665d11e4c58ef2598554bdb0a880b2ce69807a
adrian <eekkaaadrian@gmail.com>
2018-12-02 01:18:20 +0700

    add bab1 and bab2

---
+++ LightweightDjango/bab2/placeholder_caching.py
@@ -0,0 +1,94 @@
+import hashlib
+import os
+import sys
+
+from django.conf import settings
+
+DEBUG = False
+
+SECRET_KEY = os.environ.get('SECRET_KEY', '%jv_4#hoaqwig2gu!eg#^ozptd*a@88u(aasv7z!7xt^5(*i&k')
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+
+settings.configure(
+	DEBUG=DEBUG,
+	SECRET_KEY=SECRET_KEY,
+	ALLOWED_HOSTS=ALLOWED_HOSTS,
+	ROOT_URLCONF=__name__,
+	MIDDLEWARE_CLASSES=(
+		'django.middleware.common.CommonMiddleware',
+		'django.middleware.csrf.CsrfViewMiddleware',
+		'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	),
+)
+from django.conf.urls import url
+from django.core.wsgi import get_wsgi_application
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.views.decorators.http import etag 
+
+def index(request):
+	return HttpResponse('Hello World')
+
+from django import forms
+from django.conf.urls import url
+from django.core.cache import cache
+from django.core.urlresolvers import reverse 
+from django.core.wsgi import get_wsgi_application
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.shortcuts import render 
+from io import BytesIO
+from PIL import Image, ImageDraw
+
+class ImageForm(forms.Form):
+	"""Form to validate requested placeholder image."""
+	height = forms.IntegerField(min_value=1, max_value=2000)
+	width = forms.IntegerField(min_value=1, max_value=2000)
+
+	def generate(self, image_format='PNG'):
+		"""Generate an image of the given type and return as raw bytes."""
+		height = self.cleaned_data['height']
+		width = self.cleaned_data['width']
+		key = '{}.{}.{}'.format(width, height, image_format)
+		content = cache.get(key)
+		if content is None:
+			image = Image.new('RGB', (width, height))
+			draw = ImageDraw.Draw(image)
+			text = '{} X {}'.format(width, height)
+			textwidth, textheight = draw.textsize(text)
+			if textwidth < width and textheight < height:
+				texttop = (height - textheight) // 2
+				textleft = (width - textwidth) // 2
+				draw.text((textleft, texttop), text, fill=(200, 255, 255))
+			content = BytesIO()
+			image.save(content, image_format)
+			content.seek(0)
+			cache.set(key, content, 60*60)
+		return content
+
+def generate_etag(request, width, height):
+	content = 'Placeholder: {0} x {1}'.format(width, height)
+        print content
+	return hashlib.sha1(content.encode('utf-8')).hexdigest()
+
+@etag(generate_etag) 
+def placeholder(request, width, height):
+	form = ImageForm({'height': height, 'width': width})
+	if form.is_valid():
+		height = form.cleaned_data['height']
+		width = form.cleaned_data['width']
+		# TODO: Generate image of requested size
+		image = form.generate() 
+		return HttpResponse(image, content_type='image/png')
+	else:
+		return HttpResponseBadRequest('Invalid Image Request')
+
+urlpatterns = (
+	url(r'^image/(?P<width>[0-9]+)x(?P<height>[0-9]+)/$', placeholder, name='placeholder'),
+	url(r'^index/$', index),
+)
+
+application = get_wsgi_application()
+
+if __name__ == "__main__":
+ from django.core.management import execute_from_command_line
+ execute_from_command_line(sys.argv)
