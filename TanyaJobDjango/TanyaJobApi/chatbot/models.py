7bed509b8f10be95270698ca10e2a9eeda19d6c3
adrian <eekkaaadrian@gmail.com>
2018-12-30 13:38:24 +0700

    add many things

--- TanyaJobDjango/TanyaJobApi/chatbot/models.py
+++ TanyaJobDjango/TanyaJobApi/chatbot/models.py
@@ -8,13 +8,19 @@ from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 
 class UserAnswer(models.Model):
-	category = models.CharField(max_length=255, blank=True, default='')
-	text = models.TextField(blank=True, default='')
-	def __str__(self):
-		return self.text
+    category = models.CharField(max_length=255, blank=True, default='')
+    text = models.CharField(blank=False, default='',  max_length=255)
+
+    class Meta:
+        unique_together = ('category', 'text')
+    def __str__(self):
+        return self.text
 
 class BotQuestion(models.Model):
-	category = models.CharField(max_length=255, blank=True, default='')
-	text = models.TextField(blank=True, default='')
-	def __str__(self):
-		return self.text
+    category = models.CharField(max_length=255, blank=True, default='')
+    text = models.CharField(blank=False, default='', max_length=255)
+        
+    class Meta:
+        unique_together = ('category', 'text')
+    def __str__(self):
+        return self.text
