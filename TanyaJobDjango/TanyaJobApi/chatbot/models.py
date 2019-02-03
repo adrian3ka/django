219c99eade84806387c8d729492506e7bd2f4558
adrian <eekkaaadrian@gmail.com>
2019-02-03 12:46:16 +0700

    make migrations and remove pyc

--- TanyaJobDjango/TanyaJobApi/chatbot/models.py
+++ TanyaJobDjango/TanyaJobApi/chatbot/models.py
@@ -4,7 +4,40 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+class MasterDegree(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100, blank=False, null=False, unique=True)
 
+class MasterFacility(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100, blank=False, null=False, unique=True)
+
+class MasterField(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100, blank=False, null=False, unique=True)
+
+class MasterIndustry(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100, blank=False, null=False, unique=True)
+
+class MasterJobLevel(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100, blank=False, null=False, unique=True)
+
+class MasterLocation(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100, blank=False, null=False, unique=True)
+
+class MasterMajor(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100, blank=False, null=False, unique=True)
+
+class MasterSkillSet(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100, blank=False, null=False, unique=True)
+
+    def __str__(self):
+        return self.name.title()
 # Create your models here.
 
 class UserAnswer(models.Model):
