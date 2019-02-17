37d9c9a4d12e518d7fef88ac43bf40d427348146
adrian <eekkaaadrian@gmail.com>
2019-02-17 13:54:48 +0700

    yapf all

--- TanyaJobDjango/TanyaJobApi/job/migrations/0001_initial.py
+++ TanyaJobDjango/TanyaJobApi/job/migrations/0001_initial.py
@@ -9,22 +9,33 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
             name='Job',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(blank=True, default='', max_length=255)),
-                ('degree', models.CharField(blank=True, default='', max_length=255)),
-                ('major', models.CharField(blank=True, default='', max_length=255)),
-                ('industry', models.CharField(blank=True, default='', max_length=255)),
+                ('id',
+                 models.AutoField(
+                     auto_created=True,
+                     primary_key=True,
+                     serialize=False,
+                     verbose_name='ID')),
+                ('title',
+                 models.CharField(blank=True, default='', max_length=255)),
+                ('degree',
+                 models.CharField(blank=True, default='', max_length=255)),
+                ('major',
+                 models.CharField(blank=True, default='', max_length=255)),
+                ('industry',
+                 models.CharField(blank=True, default='', max_length=255)),
                 ('age', models.SmallIntegerField(default=55)),
-                ('field', models.CharField(blank=True, default='', max_length=255)),
-                ('location', models.CharField(blank=True, default='', max_length=255)),
-                ('job_level', models.CharField(blank=True, default='', max_length=255)),
+                ('field',
+                 models.CharField(blank=True, default='', max_length=255)),
+                ('location',
+                 models.CharField(blank=True, default='', max_length=255)),
+                ('job_level',
+                 models.CharField(blank=True, default='', max_length=255)),
                 ('work_exp', models.BigIntegerField(blank=True, default=0)),
                 ('min_salary', models.BigIntegerField(blank=True, default=0)),
                 ('max_salary', models.BigIntegerField(blank=True, default=0)),
