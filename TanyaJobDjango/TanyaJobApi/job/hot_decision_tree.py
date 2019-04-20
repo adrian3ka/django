eee6da70451b667544f6ecc6cb53b279a1758b3f
adrian <eekkaaadrian@gmail.com>
2019-04-21 02:40:49 +0700

    tree count = 50

--- TanyaJobDjango/TanyaJobApi/job/hot_decision_tree.py
+++ TanyaJobDjango/TanyaJobApi/job/hot_decision_tree.py
@@ -35,9 +35,9 @@ class HotJobRecommendationDecisionTree:
     clf = tree.DecisionTreeClassifier()
 
     MINIMUM_AGREED_TREE = 3
-    TREE_COUNT = 45
+    TREE_COUNT = 50
     MAX_DEPTH = 700
-    PARALEL_ESTIMATOR = TREE_COUNT / 10
+    PARALEL_ESTIMATOR = TREE_COUNT / 5
     THRESHOLD = float(MINIMUM_AGREED_TREE) / float(TREE_COUNT)
 
     jobDatas = []  # other from title
